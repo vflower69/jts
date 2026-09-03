606e427c8789a326f78c53fa3d24056557f2d73c
vflower69 <88990196+vflower69@users.noreply.github.com>
2026-09-02 20:35:21 -0700

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -204,8 +204,8 @@ async function submitFormSighting() {
   //const timestamp = new Date().toISOString();//for testing only
   const timestamp=new Date(time).toISOString();
   const payload = { lat, lng, timestamp, note };
-  console.log(payload);
-  console.log(JSON.stringify(payload));
+  //console.log(payload); //test only
+  //console.log(JSON.stringify(payload)); //test only
   //const payload = JSON.parse(JSON.stringify({ lat, lng, timestamp, note })); //Force a clean object. This strips any inherited keys, like, constructor.
 
   try {
